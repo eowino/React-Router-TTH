d1de0c608f73ae087d111457e4cbdedf7de250b5
Evans Owino <e.owino@reply.com>
2017-08-08 11:31:36 +0100

    routes func

--- src/components/courses/CSS.js
+++ src/components/courses/CSS.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import Course from './Course';
-import { CSSCourses } from '../../data/courses';
+import React from "react";
+import Course from "./Course";
+import { CSSCourses } from "../../data/courses";
 
-const CSS = (props) => {
-  let courses = CSSCourses.map((course) => {
-    return <Course title={course.title}
-                   desc={course.description}
-                   img={course.img_src}
-                   key={course.id} />
-  }); 
+const CSS = props => {
+  let courses = CSSCourses.map(course => {
+    return (
+      <Course
+        title={course.title}
+        desc={course.description}
+        img={course.img_src}
+        key={course.id}
+      />
+    );
+  });
   return (
     <div>
       <ul>
-        {courses}    
+        {courses}
       </ul>
     </div>
   );
-}
+};
 
 export default CSS;
